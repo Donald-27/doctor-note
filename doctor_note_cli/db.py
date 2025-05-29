1d66ad80711170238d294733247d34ebec8552ab
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-30 00:32:13 +0300

    set up sqlalchemy and add db connection in db.py

--- doctor_note_cli/db.py
+++ doctor_note_cli/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-engine = create_engine("sqlite:///doctor_notes.db")  # local sqlite file
+engine = create_engine("sqlite:///doctor_notes.db")
 Session = sessionmaker(bind=engine)
 
 def init_db():
