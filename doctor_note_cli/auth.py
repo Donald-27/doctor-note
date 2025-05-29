ff78320174bc84dd7851e22a50975815b1267022
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-30 00:28:45 +0300

    register and login logic

--- doctor_note_cli/auth.py
+++ doctor_note_cli/auth.py
@@ -3,7 +3,6 @@ import bcrypt
 from .models import User
 def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
 def check_password(password, hashed):
     return bcrypt.checkpw(password.encode('utf-8'), hashed)
 
