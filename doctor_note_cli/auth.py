d52c375e7c27435e6c91a998e1ef24dd9e0039c6
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-30 11:56:08 +0300

    modified auth.py to encode hashed password as utf-8 before storing

--- doctor_note_cli/auth.py
+++ doctor_note_cli/auth.py
@@ -4,7 +4,7 @@ from models import User
 
 def hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    return hashed.decode('utf-8')  # Store as string in DB
+    return hashed.decode('utf-8')
 
 def check_password(password: str, hashed: str) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
